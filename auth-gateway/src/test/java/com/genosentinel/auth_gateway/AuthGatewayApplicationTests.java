a4b7a906ad8ac4155909d3370f84c06f69fabbb3
SDuque28 <santiago.duquer@autonoma.edu.co>
2025-11-11 13:51:40 -0500

    feat(auth-gateway): Creation of the SpringBoot proyect

---
+++ auth-gateway/src/test/java/com/genosentinel/auth_gateway/AuthGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genosentinel.auth_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
